ff84827c607ec34ced98895dd2886fc6c16f955a
kig6022 <kig3534@gmail.com>
2021-08-23 11:22:31 +0900

    알고리즘100제 최초커밋

---
+++ src/algorithm100No/Algorithm100Numbers.java
@@ -0,0 +1,7 @@
+package algorithm100No;
+
+public class Algorithm100Numbers {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/algorithm100No/AlgorithmNo1.java
@@ -0,0 +1,5 @@
+package algorithm100No;
+
+public class AlgorithmNo1 {
+	
+}
