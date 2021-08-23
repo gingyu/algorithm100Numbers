a6f78ce7d3e2b506579abb09e2b36b2f5887a55b
kig3534 <kig3534@gmail.com>
2021-08-23 14:17:40 +0900

    2번문제 피보나치 수열을 출력하시오.

--- src/algorithm100No/Algorithm100Numbers.java
+++ src/algorithm100No/Algorithm100Numbers.java
@@ -1,11 +1,16 @@
 package algorithm100No;
 
 import algorithm100No.algorithmNo001.AlgorithmNo1;
+import algorithm100No.algorithmNo002.AlgorithmNo002;
 
 public class Algorithm100Numbers {
 	public static void main(String[] args) {
 		// 1번문제 학생이름과 학번을 입력 후 검색하는 프로그램
 		//AlgorithmNo1.student_me();
 		//AlgorithmNo1.student_jins();
+		
+		// 2번문제 피보나치 수열을 출력하시오
+		//AlgorithmNo002.fibonacci_me();
+		//AlgorithmNo002.fibonacci_jins();
 	}
 }
